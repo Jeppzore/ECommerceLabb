60cff190d7edf068d4bbede5fde2b6b96b68e57f
Jesper Johansson <jesalv0511@hotmail.com>
2025-03-23 19:33:55 +0100

    update program.cs
    Removed some unncessecary comments

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!) });
-//builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<CustomerService>();
 builder.Services.AddScoped<OrderService>();
@@ -29,9 +27,9 @@ builder.Services.AddHttpClient<CustomerService>(client =>
 
     client.BaseAddress = new Uri(apiBaseUrl);
     Console.WriteLine($"HttpClient CustomerService BaseAddress SET: {client.BaseAddress}");
-    //client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!);
-    //Console.WriteLine($"HttpClient CustomerService BaseAddress: {client.BaseAddress}");
 });
+
+
 
 
 var app = builder.Build();
